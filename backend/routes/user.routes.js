d0309368326bc6d4595ad54f4a4eb6bdedb407bb
Vedantghole06 <Vedantghole06@gmail.com>
2025-05-20 21:39:25 +0530

    update user.routes.js change update route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -10,18 +10,25 @@ const router = express.Router();
 router.post('/create', createUser);
 // The createUser function is called when a POST request is made to the /create endpoint.
 
+
 // Route to delete a user by ID
 router.delete('/deleteUser/:id', deleteUser);
+// The deleteUser function is called when a DELETE request is made to the /deleteUser/:id endpoint.
+
 
 // Route to get all users
-router.get('users/', getAllUsers);
+router.get('/users/', getAllUsers);
+// The getAllUsers function is called when a GET request is made to the /users/ endpoint.
+
 
 // Route to get one user by ID
-router.get('users/:id', getUserById);
+router.get('/users/:id', getUserById);
+// The getUserById function is called when a GET request is made to the /users/:id endpoint.
+
 
 // Route to update a user by ID
-router.put('users/:id', updateUser);
-
+router.put('/updateUser/:id', updateUser);
+// The updateUser function is called when a PUT request is made to the /updateUser/:id endpoint.
 
 
 export default router;
